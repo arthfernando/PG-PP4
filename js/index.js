6ac3e9c3c2a1465f96cfd34561eefd0b28d0c8db
Guilherme Gonçalves <guilhermeqg0@gmail.com>
2021-11-06 17:21:57 -0300

    Adição da esfera

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 document.addEventListener("click", () => {
     boxmaterial.wireframe = !boxmaterial.wireframe;
+    spherematerial.wireframe = !spherematerial.wireframe;
+
 });
 
 
@@ -11,6 +13,8 @@ function animate() {
     cube.rotation.x += 0.02;
     cube.rotation.y += 0.02;
     cube.rotation.z += 0.02;
+
+    sphere.rotation.x += 0.02;
 
     renderer.render( scene, camera );
 }
@@ -31,9 +35,16 @@ const cube = new THREE.Mesh( boxgeometry, boxmaterial );
 cube.scale.set(0.4,0.9,0.9);
 cube.position.set(-2,-2,0);
 
+// Guilherme Gonçalves
+const spheregeometry = new THREE.SphereGeometry( 1, 16, 16 );
+const spherematerial = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+spherematerial.wireframe = true;
+const sphere = new THREE.Mesh( spheregeometry, spherematerial );
+sphere.scale.set(0.9,0.9,0.9);
+sphere.position.set(2,2,0);
 
 camera.position.z = 5;
 
-scene.add( cube );
+scene.add( cube, sphere );
 
 animate();
