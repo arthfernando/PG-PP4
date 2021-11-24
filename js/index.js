fc2e3a4e5f8306ce12f1cf87e0f9725f8a61b551
Arthur <arthur.fernando03@gmail.com>
2021-11-24 20:43:36 -0300

    orbit inserted

--- js/index.js
+++ js/index.js
@@ -1,15 +1,60 @@
-document.addEventListener("click", () => {
+const boxBtn = document.getElementById("btnBox").addEventListener("click", (e) => {
+    e.preventDefault();
     boxmaterial.wireframe = !boxmaterial.wireframe;
+});
+
+const sphereBtn = document.getElementById("btnSphere").addEventListener("click", (e) => {
+    e.preventDefault();
     spherematerial.wireframe = !spherematerial.wireframe;
+});
+
+const torusBtn = document.getElementById("btnTorus").addEventListener("click", (e) => {
+    e.preventDefault();
     torusmaterial.wireframe = !torusmaterial.wireframe;
+});
+
+const d20Btn = document.getElementById("btnD20").addEventListener("click", (e) => {
+    e.preventDefault();
     d20material.wireframe = !d20material.wireframe;
 });
 
-var d20pulse = 1;
+const switchCam = document.getElementById("btnSwitchCam").addEventListener("click", (e) => {
+    e.preventDefault();
+    if(isCamera1) {
+        renderCamera = camera2;
+    } else {
+        renderCamera = camera;
+    }
+    isCamera1 = !isCamera1;
+});
 
-function animate() {
 
-    requestAnimationFrame( animate );
+const cameraBtn = document.getElementById("btnCamera").addEventListener("click", (e) => {
+    e.preventDefault();
+    if(!isCamera1) {
+        return;
+    }
+    if(isPerspective) {
+        camera = new THREE.OrthographicCamera( window.innerWidth/-50 , window.innerWidth/50, window.innerHeight/50, window.innerHeight/-50, 0.1, 1000 );
+        camera.position.z = 5;
+        camera.zoom = 2;
+        camera.updateProjectionMatrix();
+    } else {
+        camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 0.1, 1000 );
+        camera.position.z = 5;
+    }
+
+    isPerspective = !isPerspective;
+    renderCamera = camera;
+});
+
+
+
+function animate(now) {
+    now *= 0.001;
+    const delta = now - then;
+    then = now;
+    // requestAnimationFrame( animate );
 
     cube.rotation.x += 0.02;
     cube.rotation.y += 0.02;
@@ -20,16 +65,39 @@ function animate() {
     torusKnot.rotation.y += 0.02;
     torusKnot.rotation.z += 0.02;
 
+    // delta retirado de: https://stackoverflow.com/questions/45343673/three-js-animate-in-real-time
     d20.rotation.y += 0.02;
-    d20.scale.set(d20pulse%3, d20pulse%3, d20pulse%3);
+    d20.scale.x += Math.sin(d20pulse)*delta*5;
+    d20.scale.y += Math.sin(d20pulse)*delta*5;
+    d20.scale.z += Math.sin(d20pulse)*delta*5;
     d20pulse++;
 
-    renderer.render( scene, camera );
+    date = Date.now() * 0.001;
+    sphere.position.set(
+        Math.cos(date) * orbitRadius,
+        0,
+        Math.sin(date) * orbitRadius
+    );
+
+    renderer.render( scene, renderCamera );
+    requestAnimationFrame( animate );
+
 }
 
+let isPerspective;
+let isCamera1;
+let renderCamera;
+let d20pulse = 1;
+
+let then = 0;
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 0.1, 1000 );
+let camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 0.1, 1000 );
+isCamera1 = true;
+renderCamera = camera;
+let camera2 = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 0.1, 1000 );
+isPerspective = true;
+// const camera = new THREE.OrthographicCamera( width / - 2, width / 2, height / 2, height / - 2, 1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -41,7 +109,7 @@ const boxmaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 boxmaterial.wireframe = true;
 const cube = new THREE.Mesh( boxgeometry, boxmaterial );
 cube.scale.set(0.4,0.9,0.9);
-cube.position.set(-2,-2,0);
+cube.position.set(-3,0,0);
 
 // Guilherme Gonçalves
 const spheregeometry = new THREE.SphereGeometry( 1, 16, 16 );
@@ -49,7 +117,7 @@ const spherematerial = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
 spherematerial.wireframe = true;
 const sphere = new THREE.Mesh( spheregeometry, spherematerial );
 sphere.scale.set(0.9,0.9,0.9);
-sphere.position.set(2,2,0);
+sphere.position.set(3,0,0);
 
 // Gabriela
 const torusgeometry = new THREE.TorusKnotGeometry( 1, 0.4, 64, 8 );
@@ -57,7 +125,7 @@ const torusmaterial = new THREE.MeshBasicMaterial( { color: 0xD6D7FF } );
 torusmaterial.wireframe = true;
 const torusKnot = new THREE.Mesh( torusgeometry, torusmaterial );
 torusKnot.scale.set(0.9,0.9,0.9);
-torusKnot.position.set(-2,2,0);
+torusKnot.position.set(0,3,0);
 
 //Guilherme Cosimato
 const d20geometry = new THREE.IcosahedronGeometry(1,0);
@@ -65,10 +133,21 @@ const d20material = new THREE.MeshBasicMaterial( { color: 0xf20303 } );
 d20material.wireframe = true;
 const d20 = new THREE.Mesh( d20geometry, d20material );
 d20.scale.set(0.9,0.9,0.9);
-d20.position.set(2,-2,0);
+d20.position.set(0,-3,0);
 
 camera.position.z = 5;
 
+camera2.position.z = 5;
+camera2.position.x = 2;
+camera2.rotation.y = Math.PI/4;
+
+var orbitRadius = 10; // for example
+var date;
+
+
+
 scene.add( cube, sphere, torusKnot, d20 );
 
-animate();
+// animate();
+requestAnimationFrame( animate );
+
